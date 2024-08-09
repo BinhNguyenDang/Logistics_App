437ac47d410fd07f78757a2a879780366810c2b8
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-08-09 16:56:07 +0700

    fix added locale

---
+++ config/initializers/locale.rb
@@ -0,0 +1,6 @@
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+# Permitted locales available for the application
+I18n.available_locales = %i[:en, :'vi']
+# Set default locale to something other than :en
+config.i18n.default_locale = :'vi'
