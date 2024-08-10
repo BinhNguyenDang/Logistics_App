1232c72de1a3f530b68f2b3b3ed2daaa072c4b65
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-08-10 16:04:56 +0700

    test(mode): added view tests(Fix Potential Capybara Issues)

---
+++ spec/system/modes/view_mode_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe "View modes" do 
+  it "View modes" do 
+    #Act 
+    visit root_path
+    click_on "Modes"
+    #Assert
+    expect(page).to have_current_path(modes_path)
+  end
+  it "Successfully" do 
+    #Arrange 
+    for i in 1..3
+      Mode.create!(
+        name: "Mode #{i}",
+        min_distance: 1 * i,
+        max_distance: 20 * i,
+        min_weight: 1 * i,
+        max_weight: 20 * i,
+        fixed_fee: 5 * i,
+        active: true
+      )
+    end
+    #Act 
+    visit root_path
+    click_on "Modes"
+    #Assert
+    expect(page).to have_current_path(modes_path)
+    within('table thead') do 
+      expect(page).to have_content "Name"
+      expect(page).to have_content "Distance"
+    end
+    within('table tbody') do 
+      expect(page).to have_content "Mode 1"
+      expect(page).to have_content "Mode 2"
+      expect(page).to have_content "Mode 3"
+      expect(page).to have_content "From 1 to 20"
+      expect(page).to have_content "From 2 to 40"
+      expect(page).to have_content "From 3 to 60"
+    end
+  end
+  it "no registered modes" do 
+    #Act 
+    visit root_path
+    click_on "Modes"
+    #Assert
+    expect(page).to have_current_path(modes_path)
+    expect(page).to have_content "No modes registered"
+  end
+end
